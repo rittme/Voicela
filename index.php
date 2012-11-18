8ebfb2e3ed384664078dbe2d4e39c6d5710258ec
Bernardo Rittmeyer <bernardo@rittme.com>
2012-11-18 13:21:53 +0100

    pdfsgijsdpgjj

--- index.php
+++ index.php
@@ -37,15 +37,15 @@
                 $result = $db->where('idVIP',$matches[1])->get('VIP',1);
                 $realisateur = $db->where('realisateur',$matches[1])->get('film');
 
-                $params = array($matches[1]);
+                /*$params = array($matches[1]);
                 $acteur = $db->rawQuery("SELECT * FROM joue, film WHERE joue.idfilm = film.idfilm AND joue.idVIP = ?", $params);
                 $photo  = $db->rawQuery("SELECT * FROM vip_photo, photo WHERE photo.idphoto = vip_photo.idphoto AND vip_photo.idVIP = ?", $params);
-
+                */
                 $data = array();
                 $data["vip"]         = $result[0];
-                $data["acteur"]      = $acteur;
+                //$data["acteur"]      = $acteur;
                 $data["realisateur"] = $realisateur;
-                $data["photo"]       = $photo;
+                //$data["photo"]       = $photo;
 
                 if(!empty($result) && count($result) > 0) {      
                     SimplestView::render("header");      
